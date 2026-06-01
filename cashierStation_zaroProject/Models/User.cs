74a3d5d6d8e1c1c3016dd6deb09c0b5b4d99f55e
ZalanMede <medezalan@turr.hu>
2026-06-01 09:39:39 +0200

    User, Roles class elkészítve

---
+++ cashierStation_zaroProject/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cashierStation_zaroProject.Models;
+
+namespace cashierStation_zaroProject.Models
+{
+    internal class User
+    {
+        public User()
+        {
+        }
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string RoleNev => Enum.GetName(typeof(Role), Role) ?? "Unknown";
+
+        public User(string username, string fullName, int role)
+        {
+            Username = username;
+            FullName = fullName;
+            Role = role;
+        }
+
+        public User(string username, string fullName, string password, int role)
+        {
+            Username = username;
+            FullName = fullName;
+            Password = password;
+            Role = role;
+        }
+    }
+}
---
+++ cashierStation_zaroProject/Services/PassManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cashierStation_zaroProject.Services
+{
+    internal class PassManager
+    {
+    }
+}
