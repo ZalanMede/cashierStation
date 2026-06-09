1f7ae8b1a3222da19584845cfc32809722060b27
ZalanMede <medezalan@turr.hu>
2026-06-09 13:25:16 +0200

    State enum kész, datagrid elkezdve, numpad bővítve, fizetési elemek a helyükön

--- cashierStation_zaroProject/StationControllerWindow.xaml.cs
+++ cashierStation_zaroProject/StationControllerWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using cashierStation_zaroProject.Models;
 
 namespace cashierStation_zaroProject
 {
@@ -19,27 +20,70 @@ namespace cashierStation_zaroProject
     /// </summary>
     public partial class StationControllerWindow : Window
     {
+        public SystemSate sysState;
         public StationControllerWindow()
         {
             InitializeComponent();
+            sysState = SystemSate.ItemManagement;
         }
 
         private void NumButton_Click(object sender, RoutedEventArgs e)
         {
             Button senderBtn = (Button)sender;
             string hitNum = (string)senderBtn.Content;
+            if (hitNum == "●")
+            {
+                hitNum = ".";
+            }
 
             numInputTxtBox.Text += hitNum.ToString();
         }
 
         private void numEnter_Click(object sender, RoutedEventArgs e)
         {
-
+            switch (sysState)
+            {
+                case SystemSate.ItemManagement:
+                    break;
+                case SystemSate.CashIn:
+                    break;
+                case SystemSate.CreditIn:
+                    break;
+                case SystemSate.OtherIn:
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void numDel_Click(object sender, RoutedEventArgs e)
         {
-            numInputTxtBox.Text = numInputTxtBox.Text.Remove(numInputTxtBox.Text.Length - 1);
+            if (numInputTxtBox.Text.Length > 0)
+            {
+                numInputTxtBox.Text = numInputTxtBox.Text.Remove(numInputTxtBox.Text.Length - 1);
+            }
+        }
+
+        private void numCancelAll_Click(object sender, RoutedEventArgs e)
+        {
+            sysState = SystemSate.ItemManagement;
+            payRec.Text = "0.00 HUF";
+            payChange.Text = "0.00 HUF";
+        }
+
+        private void paymentCash_Click(object sender, RoutedEventArgs e)
+        {
+            sysState = SystemSate.CashIn;
+        }
+
+        private void paymentCredit_Click(object sender, RoutedEventArgs e)
+        {
+            sysState = SystemSate.CreditIn;
+        }
+
+        private void paymentOther_Click(object sender, RoutedEventArgs e)
+        {
+            sysState = SystemSate.OtherIn;
         }
     }
 }
